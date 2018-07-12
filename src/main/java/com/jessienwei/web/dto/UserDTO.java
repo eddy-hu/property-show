8164dab10ee44feb6247ca39565aadce6f253314
Louis Gu <gu000031@algonquinlive.com>
2018-07-12 17:06:22 -0400

    add com.jessienwei.web.controller -- UserController.java com.jessienwei.web.exception  -- ResourceNotFoundException.java com.jessienwei.web.repository -- UserRepository.java src/maim/resources -- application.properties
    modify
    com.jessienwei.web.dto -- UserDTO.java
    com.jessienwei.web     -- Application.java
    build.gradle
    pom.xml

--- src/main/java/com/jessienwei/web/dto/UserDTO.java
+++ src/main/java/com/jessienwei/web/dto/UserDTO.java
@@ -1,67 +1,99 @@
 package com.jessienwei.web.dto;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import javax.validation.constraints.NotBlank;
+import javax.persistence.*;
 
-public class UserDTO {
-//	@NotNull
-//	@NotEmpty
-//	private String firstName;
-//	@NotNull
-//	@NotEmpty
-//	private String lastName;
-//	@NotNull
-//	@NotEmpty
-//	private String password;
-//	private String matchingPassword;
-//	@NotNull
-//	@NotEmpty
-//	private String email;
-//
-//	public String getFirstName() {
-//		return firstName;
-//	}
-//
-//	public void setFirstName(String firstName) {
-//		this.firstName = firstName;
-//	}
-//
-//	public String getLastName() {
-//		return lastName;
-//	}
-//
-//	public void setLastName(String lastName) {
-//		this.lastName = lastName;
-//	}
-//
-//	public String getPassword() {
-//		return password;
-//	}
-//
-//	public void setPassword(String password) {
-//		this.password = password;
-//	}
-//
-//	public String getMatchingPassword() {
-//		return matchingPassword;
-//	}
-//
-//	public void setMatchingPassword(String matchingPassword) {
-//		this.matchingPassword = matchingPassword;
-//	}
-//
-//	public String getEmail() {
-//		return email;
-//	}
-//
-//	public void setEmail(String email) {
-//		this.email = email;
-//	}
-//
-//	@Override
-//	public String toString() {
-//		return "UserDTO [firstName=" + firstName + ", lastName=" + lastName + ", password=" + password
-//				+ ", matchingPassword=" + matchingPassword + ", email=" + email + "]";
-//	}
+import java.io.Serializable;
+import java.util.Date;
 
+
+@Entity
+@Table(name = "user")
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(value = {"createdAt", "updatedAt"}, allowGetters = true)
+public class UserDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	
+	@Column(name="fname")
+	@NotBlank
+	private String firstName;
+	
+	@Column(name="lname")
+	@NotBlank
+	private String lastName;
+	
+	@Column(name="pswd")
+	@NotBlank
+	private String password;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="phone")
+	private String phone;
+	
+	@Column(nullable = false, updatable = false, name="created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createdAt;
+
+    @Column(nullable = false, name="updated_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updatedAt;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserDTO [firstName=" + firstName + ", lastName=" + lastName + ", password=" + password + 
+			   ", email=" + email + ", phone=" + phone + "]";
+	}
 }
